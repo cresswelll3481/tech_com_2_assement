ae262e1aab38ba70641442585a8faecca34b3f40
cresswelll3481 <cresswelll3481@masseyhigh.school.nz>
2026-06-08 15:07:12 +1200

    Add files via upload

--- Assessment/main_tech_com.py
+++ Assessment/main_tech_com.py
@@ -1,6 +1,6 @@
 import pandas
 import math
-def string_checker(question, valid_ans_list, num_letters):
+def string_checker(question, valid_ans_list, num_letters = 1):
     """checks if the user has input a valid answer from a list and also allows for """
     while True:
         response = input(question).lower()
@@ -14,7 +14,7 @@ def string_checker(question, valid_ans_list, num_letters):
 def make_statement(statement, decoration):
     """formats a statement to make it look appealing to the user"""
     print(f"{decoration * 3} {statement} {decoration * 3}")
-def num_check(question, num_type, exit_code=None):
+def num_check(question, num_type, exit_code='xxx'):
     """checks if the users number is more than 0"""
     if num_type == "integer":
         error = "Please enter a integer more than zero"
@@ -45,89 +45,105 @@ instructions = ("first input how many questions you would like to solve"
                 "")
 # initialise num_questions
 num_questions = 0
-#initalise lists/ pandas lists
+# initialise lists/ pandas lists
 yes_no = ['yes', 'no']
-area_or_perimeter = ['area', 'perimeter']
-shapes = ['square', 'triangle', 'rectangle', 'circle']
+area_or_perimeter = ['area', 'perimeter', 'xxx']
+shapes = ['square', 'triangle', 'rectangle', 'circle', 'xxx']
 ans_list = []
 num_questions_list = []
 working_list = []
+
 # pandas dictionary
 panda_dict = {
     'Question': num_questions_list,
     'Working': working_list,
     'Answer': ans_list
 }
-#prints instructions on request
+
+# prints instructions on request
 want_instructions = string_checker("Do you want to see the instructions? ",yes_no,1)
 if want_instructions == "yes":
     make_statement("Instructions", "+")
     print(instructions)
-#asks the user how many questions
+# asks the user how many questions
 how_many = num_check("How many questions do you want to solve? ", "int", "xxx")
-#runs the questions
+
+# runs the questions
 while num_questions < how_many:
     num_questions += 1
     make_statement(f"Question {num_questions}", '-')
-    what_mode = string_checker("area or perimeter? ", area_or_perimeter,1)
-    what_shape = string_checker("What shape are you trying to solve? ", shapes,2)
-    #checks if the user is working out the perimeter
+    what_mode = string_checker("area or perimeter? ", area_or_perimeter,)
+    what_shape = string_checker("What shape are you trying to solve? ", shapes,)
+
+    # checks if the user is working out the perimeter
     if what_mode == "perimeter":
-        #checks if the shape is a circle and if so then apply the right formula
+        # checks if the exit code is used during a perimeter shape selection
+        if what_shape == "xxx":
+            break
+        # checks if the shape is a circle and if so then apply the right formula
         if what_shape == "circle":
             radius = num_check("what is the radius? ", float)
             ans = 2 * math.pi * radius
-            working = f"2 x pi x {radius}"
+            working = f"2 x pi x {radius:.2f}"
+
         # checks if the shape is a triangle and if so then apply the right formula
         elif what_shape == "triangle":
            #checks if the user has all 3 sides if not gives an error, if so continues
-           have_sides = string_checker("do you have all 3 sides? ", yes_no, 1)
+           have_sides = string_checker("do you have all 3 sides? ", yes_no)
            if have_sides == "yes":
                s1 = num_check("what is side 1? ", float)
                s2 = num_check("what is side 2? ", float)
                s3 = num_check("what is side 3? ", float)
                ans = s1 + s2 + s3
-               working = f"{s1} + {s2} + {s3}"
+               working = f"{s1:.2f} + {s2:.2f} + {s3:.2f}"
            else:
                tri_error = "you cant solve this questions without all 3 sides"
                print(tri_error)
                ans = "n/a"
                working = "n/a"
+
         # checks if the shape is a square and if so then apply the right formula
         elif what_shape == "square":
             side = num_check("what is the base? ", float)
             ans = 4 * side
-            working = f"{side} + {side} + {side} + {side} "
+            working = f"{side:.2f} + {side:.2f} + {side:.2f} + {side:.2f} "
         # if it's not any other shape it deems it to be a rectangle
         else:
             base = num_check("what is the base? ", float)
             height = num_check("what is the height? ", float)
             ans = 2 * height + 2 * base
-            working = f"{height} + {base} + {height} + {base} "
+            working = f"{height:.2f} + {base:.2f} + {height:.2f} + {base:.2f} "
+
     #checks if it is working out the area
     if what_mode == "area":
+        # checks if the exit code is used during an area shape selection
+        if what_shape == "xxx":
+            break
         # checks if the shape is a circle and if so then apply the right formula
         if what_shape == "circle":
             radius = num_check("what is the radius? ", float)
             ans =   math.pi * (radius * radius)
-            working = f"pi x {radius} squared"
+            working = f"pi x {radius:.2f} squared"
         # checks if the shape is a triangle and if so then apply the right formula
         elif what_shape == "triangle":
             base = num_check("what is the base? ", float)
             height = num_check("what is the height? ", float)
             ans = (1/2 * base) * height
-            working = f"(1/2 * {base}) * {height}"
+            working = f"(1/2 * {base:.2f}) * {height:.2f}"
         # checks if the shape is a square and if so then apply the right formula
         elif what_shape == "square":
             side = num_check("what is a side? ", float)
             ans = side * side
-            working = f"{side} × {side}"
+            working = f"{side:.2f} × {side:.2f}"
         # if it's not any other shape it deems it to be a rectangle
         else:
             base = num_check("what is the base? ", float)
             height = num_check("what is the height? ", float)
             ans = height * base
-            working = f"{height} × {base}"
+            working = f"{height:.2f} × {base:.2f}"
+    # checks if the exit code (xxx) is used during area perimeter selection
+    elif what_mode == "xxx":
+        break
     #prints the working
     print(working)
     # prints the answer
@@ -140,7 +156,7 @@ while num_questions < how_many:
     else:
         print(f"the {what_mode} is {ans:.2f} units squared")
     #adds to a list at the end of a round
-    ans_list.append(ans)
+    ans_list.append(f"{ans:.2f}")
     num_questions_list.append(num_questions)
     working_list.append(working)
 #pandas
